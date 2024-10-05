c27057e3eda4e584741a646796eda18fce05cc87
asadmunirdev <asadmunir6645@gmail.com>
2024-10-06 01:21:41 +0500

    search bar bugs fixed

--- src/app/go-to-top/go-to-top.component.ts
+++ src/app/go-to-top/go-to-top.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-
+import { MatIconModule } from '@angular/material/icon'; // Import Material Icon Module
 @Component({
   selector: 'app-go-to-top',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule,MatIconModule],
   templateUrl: './go-to-top.component.html',
   styleUrls: ['./go-to-top.component.css']
-})export class GoToTopComponent {
+})
+export class GoToTopComponent {
   isCardVisible = false; // Flag to control card visibility
 
   // Function to toggle card visibility
@@ -21,16 +21,19 @@ import { MatIconModule } from '@angular/material/icon';
     this.isCardVisible = false; // Hide the card when any button is clicked
   }
 
+  // Scroll to the top of the page
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     this.onButtonClick(); // Close the card after action
   }
 
+  // Scroll to the bottom of the page
   scrollToBottom() {
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
     this.onButtonClick(); // Close the card after action
   }
 
+  // Go back to the previous page
   goBack() {
     if (window.history.length > 1) {
       window.history.back(); // Go back to the previous page
